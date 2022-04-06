5c92a7a362d7b31562403776f15dfca025c2a1fb
ProgrammDipper <103143334+ProgrammDipper@users.noreply.github.com>
2022-04-06 21:59:25 +0300

    Add files via upload

---
+++ BlackJek.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace AAAAAA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Приветствую вас! Введите свое количество карт");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = 0;
+
+            for (int i = 1; i <= a; ++i)
+            {
+                Console.WriteLine($"Введите номинал {i}-й карты");
+                string c = Console.ReadLine();
+
+                if (c == "J")
+                {
+                    int h = 10;
+                    b += h;
+                }
+                else
+                if (c == "Q")
+                {
+                    int h = 10;
+                    b += h;
+                }
+                else
+                if (c == "K")
+                {
+                    int h = 10;
+                    b += h;
+                }
+                else
+                if (c == "T")
+                {
+                    int h = 10;
+                    b += h;
+                }
+                else
+                {
+                    string j = Convert.ToString(c);
+                    int k = Convert.ToInt32(j);
+                    b += k;
+                }
+
+               
+
+
+
+
+            }
+            
+            Console.WriteLine($"Сумма всех ваших карт = {b}");
+        }
+    }
+}
